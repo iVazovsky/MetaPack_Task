2876c1a6e17e96050c6ba566ec2a625d2063f81a
iVazovsky <36818037+iVazovsky@users.noreply.github.com>
2018-02-25 16:11:35 +0100

    Добавьте файлы проекта.

---
+++ TaskProject1/Form1.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace TaskProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTab = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pizzaListBox = new System.Windows.Forms.CheckedListBox();
+            this.pizzaBox = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.daniaListBox = new System.Windows.Forms.CheckedListBox();
+            this.daniaBox = new System.Windows.Forms.ComboBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.zupyBox = new System.Windows.Forms.ComboBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.napojeBox = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.foodCounter = new System.Windows.Forms.NumericUpDown();
+            this.orderText = new System.Windows.Forms.TextBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.historyTab = new System.Windows.Forms.TabControl();
+            this.mainTab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.foodCounter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainTab
+            // 
+            this.mainTab.Controls.Add(this.tabPage1);
+            this.mainTab.Controls.Add(this.tabPage2);
+            this.mainTab.Controls.Add(this.tabPage3);
+            this.mainTab.Controls.Add(this.tabPage4);
+            this.mainTab.Location = new System.Drawing.Point(17, 13);
+            this.mainTab.Name = "mainTab";
+            this.mainTab.SelectedIndex = 0;
+            this.mainTab.Size = new System.Drawing.Size(268, 157);
+            this.mainTab.TabIndex = 1;
+            this.mainTab.SelectedIndexChanged += new System.EventHandler(this.mainTab_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.pizzaListBox);
+            this.tabPage1.Controls.Add(this.pizzaBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(260, 131);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Pizza";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Rodzaje pizzy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dodatki do pizzy";
+            // 
+            // pizzaListBox
+            // 
+            this.pizzaListBox.CheckOnClick = true;
+            this.pizzaListBox.FormattingEnabled = true;
+            this.pizzaListBox.Items.AddRange(new object[] {
+            "Podwójny ser - 2 zł",
+            "Salami - 2 zł",
+            "Szynka - 2 zł",
+            "Pieczarki - 2 zł"});
+            this.pizzaListBox.Location = new System.Drawing.Point(135, 29);
+            this.pizzaListBox.Name = "pizzaListBox";
+            this.pizzaListBox.Size = new System.Drawing.Size(120, 64);
+            this.pizzaListBox.TabIndex = 3;
+            // 
+            // pizzaBox
+            // 
+            this.pizzaBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.pizzaBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.pizzaBox.FormattingEnabled = true;
+            this.pizzaBox.Items.AddRange(new object[] {
+            "Margheritta - 20 zł",
+            "Vegetariana - 22 zł",
+            "Tosca - 25 zł",
+            "Venecia - 25 zł"});
+            this.pizzaBox.Location = new System.Drawing.Point(6, 29);
+            this.pizzaBox.Name = "pizzaBox";
+            this.pizzaBox.Size = new System.Drawing.Size(121, 21);
+            this.pizzaBox.TabIndex = 0;
+            this.pizzaBox.Text = "Wybierz pizzę...";
+            this.pizzaBox.SelectionChangeCommitted += new System.EventHandler(this.daniaBox_SelectionChangeCommitted);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.daniaListBox);
+            this.tabPage2.Controls.Add(this.daniaBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(260, 131);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Dania główne";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Dodatki do dań";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Rodzaje dań";
+            // 
+            // daniaListBox
+            // 
+            this.daniaListBox.CheckOnClick = true;
+            this.daniaListBox.FormattingEnabled = true;
+            this.daniaListBox.Items.AddRange(new object[] {
+            "Bar sałatkowy - 5 zł",
+            "Zestaw sosów - 6 zł"});
+            this.daniaListBox.Location = new System.Drawing.Point(6, 80);
+            this.daniaListBox.Name = "daniaListBox";
+            this.daniaListBox.Size = new System.Drawing.Size(120, 34);
+            this.daniaListBox.TabIndex = 9;
+            // 
+            // daniaBox
+            // 
+            this.daniaBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.daniaBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.daniaBox.FormattingEnabled = true;
+            this.daniaBox.Items.AddRange(new object[] {
+            "Schabowy z frytkami - 30 zł",
+            "Ryba z frytkami - 28 zł",
+            "Placek po więgiersku - 27 zł"});
+            this.daniaBox.Location = new System.Drawing.Point(6, 28);
+            this.daniaBox.Name = "daniaBox";
+            this.daniaBox.Size = new System.Drawing.Size(179, 21);
+            this.daniaBox.TabIndex = 0;
+            this.daniaBox.Text = "Wybierz danie...";
+            this.daniaBox.SelectionChangeCommitted += new System.EventHandler(this.pizzaBox_SelectionChangeCommitted);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.zupyBox);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(260, 131);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Zupy";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Rodzaje zup";
+            // 
+            // zupyBox
+            // 
+            this.zupyBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.zupyBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.zupyBox.FormattingEnabled = true;
+            this.zupyBox.Items.AddRange(new object[] {
+            "Pomidorowa - 12 zł",
+            "Rosół - 10 zł"});
+            this.zupyBox.Location = new System.Drawing.Point(6, 28);
+            this.zupyBox.Name = "zupyBox";
+            this.zupyBox.Size = new System.Drawing.Size(121, 21);
+            this.zupyBox.TabIndex = 0;
+            this.zupyBox.Tag = "";
+            this.zupyBox.Text = "Wybierz zupę...";
+            this.zupyBox.SelectionChangeCommitted += new System.EventHandler(this.zupyBox_SelectionChangeCommitted);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.label7);
+            this.tabPage4.Controls.Add(this.napojeBox);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(260, 131);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Napoje";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Rodzaje napojów";
+            // 
+            // napojeBox
+            // 
+            this.napojeBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.napojeBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.napojeBox.FormattingEnabled = true;
+            this.napojeBox.Items.AddRange(new object[] {
+            "Kawa - 5 zł",
+            "Herbata - 5 zł",
+            "Cola - 5 zł"});
+            this.napojeBox.Location = new System.Drawing.Point(7, 28);
+            this.napojeBox.Name = "napojeBox";
+            this.napojeBox.Size = new System.Drawing.Size(121, 21);
+            this.napojeBox.TabIndex = 0;
+            this.napojeBox.Text = "Wybierz napój...";
+            this.napojeBox.SelectionChangeCommitted += new System.EventHandler(this.napojeBox_SelectionChangeCommitted);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(291, 115);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "Dodaj";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(291, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Ilość";
+            // 
+            // foodCounter
+            // 
+            this.foodCounter.Location = new System.Drawing.Point(291, 63);
+            this.foodCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.foodCounter.Name = "foodCounter";
+            this.foodCounter.Size = new System.Drawing.Size(44, 20);
+            this.foodCounter.TabIndex = 2;
+            this.foodCounter.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // orderText
+            // 
+            this.orderText.Location = new System.Drawing.Point(372, 35);
+            this.orderText.Multiline = true;
+            this.orderText.Name = "orderText";
+            this.orderText.ReadOnly = true;
+            this.orderText.Size = new System.Drawing.Size(178, 568);
+            this.orderText.TabIndex = 2;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(223, 173);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(109, 13);
+            this.priceLabel.TabIndex = 3;
+            this.priceLabel.Text = "Cena do zapłaty: 0 zł";
+            // 
+            // orderButton
+            // 
+            this.orderButton.Location = new System.Drawing.Point(291, 144);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(75, 23);
+            this.orderButton.TabIndex = 8;
+            this.orderButton.Text = "Zamów";
+            this.orderButton.UseVisualStyleBackColor = true;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(414, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Twoje zamówienie";
+            // 
+            // historyTab
+            // 
+            this.historyTab.Location = new System.Drawing.Point(17, 252);
+            this.historyTab.Name = "historyTab";
+            this.historyTab.SelectedIndex = 0;
+            this.historyTab.Size = new System.Drawing.Size(268, 351);
+            this.historyTab.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 615);
+            this.Controls.Add(this.historyTab);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.orderButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.orderText);
+            this.Controls.Add(this.mainTab);
+            this.Controls.Add(this.foodCounter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "TaskProject";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainTab.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.foodCounter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl mainTab;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox pizzaListBox;
+        private System.Windows.Forms.NumericUpDown foodCounter;
+        private System.Windows.Forms.ComboBox pizzaBox;
+        private System.Windows.Forms.TextBox orderText;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox daniaListBox;
+        private System.Windows.Forms.ComboBox daniaBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox zupyBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox napojeBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TabControl historyTab;
+    }
+}
+
---
+++ TaskProject1/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace TaskProject1
+{
+    public partial class Form1 : Form
+    {
+        public string selectedFood;     // Current selected food e.g. Margheritta, Kawa etc.
+        public int totalPrice;          // Total price of all selected dishes
+        public List<string> orderList;  // List of selected main dishes
+        public List<string> addons;     // List of add-ons for selected dishes
+        public int orderID = 0;         // Order's number
+
+        public int foodCount;           // Order multiplier
+
+        public Form1()
+        {
+            orderList = new List<string>(); // Initialization of main food list
+            addons = new List<string>();    // Initialization of add-ons list
+            InitializeComponent();          
+        }
+
+        private void addButton_Click(object sender, EventArgs e)    // Function for "Dodaj" button, which adds selected dishes to the list and prints them out to special TextBox
+        {
+            foodCount = Convert.ToInt32(foodCounter.Value);
+
+            addons.AddRange(pizzaListBox.CheckedItems.Cast<string>());
+            addons.AddRange(daniaListBox.CheckedItems.Cast<string>());
+
+            for (int i = 0; i < foodCount; i++) 
+            {
+                switch (selectedFood)
+                {
+                    case "Margheritta - 20 zł":
+                        totalPrice += 20;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Vegetariana - 22 zł":
+                        totalPrice += 22;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Tosca - 25 zł":
+                        totalPrice += 25;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Venecia - 25 zł":
+                        totalPrice += 25;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Schabowy z frytkami - 30 zł":
+                        totalPrice += 30;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Ryba z frytkami - 28 zł":
+                        totalPrice += 28;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Placek po więgiersku - 27 zł":
+                        totalPrice += 27;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Pomidorowa - 12 zł":
+                        totalPrice += 12;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Rosół - 10 zł":
+                        totalPrice += 10;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Kawa - 5 zł":
+                        totalPrice += 5;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Herbata - 5 zł":
+                        totalPrice += 5;
+                        orderList.Add(selectedFood);
+                        break;
+                    case "Cola - 5 zł":
+                        totalPrice += 5;
+                        orderList.Add(selectedFood);
+                        break;
+                }
+            }
+
+            foreach (string t in orderList)
+            {
+                orderText.Text += (t + "\r\n");
+
+                foreach(string s in addons)
+                {
+                    totalPrice += 2;
+
+                    if (s == "Bar sałatkowy - 5 zł")
+                        totalPrice += 3;
+                    if (s == "Zestaw sosów - 6 zł")
+                        totalPrice += 4;
+
+                    orderText.Text += ("  -" + s + "\r\n");                    
+                }
+            }
+
+            priceLabel.Text = ("Cena do zapłaty: " + totalPrice + " zł");
+
+            
+            orderList.Clear();
+            addons.Clear();
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)  // Function for "Zamów" button, which sends e-mail to the adress given in settings, as well as prints out order history
+        {
+            orderID++;
+
+            if (orderText.Text != "")
+            {
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    SmtpClient SmtpServer = new SmtpClient(Properties.Settings.Default.smtp);
+
+                    mail.From = new MailAddress(Properties.Settings.Default.nadawca);
+                    mail.To.Add(Properties.Settings.Default.odbiorca);
+                    mail.Subject = ("Order #" + orderID);
+                    mail.Body = (orderText.Text + "\r\n" + priceLabel.Text);
+
+                    SmtpServer.Port = 587;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential(Properties.Settings.Default.username, Properties.Settings.Default.password);
+                    SmtpServer.EnableSsl = true;
+
+                    SmtpServer.Send(mail);
+                    MessageBox.Show("Your order has been successfully sent!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+                TabPage historyPage = new TabPage("Order #" + orderID);
+                historyTab.TabPages.Add(historyPage);
+
+                TextBox historyText = new TextBox();
+                historyText.Dock = DockStyle.Fill;
+                historyText.Multiline = true;
+                historyText.Text = (orderText.Text + "\r\n" + priceLabel.Text);
+
+                historyPage.Controls.Add(historyText);
+
+                historyPage.Name += orderID;
+                historyText.Name += orderID;
+
+                orderText.Clear();
+                clearAll();
+            }
+        }
+
+        private void pizzaBox_SelectionChangeCommitted(object sender, EventArgs e)  //// Functions that overwrites selected dish depending on element in which it was selected
+        {
+            selectedFood = daniaBox.SelectedItem.ToString();            
+        }
+
+        private void daniaBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            selectedFood = pizzaBox.SelectedItem.ToString();
+        }
+
+        private void zupyBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            selectedFood = zupyBox.SelectedItem.ToString();
+        }
+
+        private void napojeBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            selectedFood = napojeBox.SelectedItem.ToString();
+        }                                                                          ////
+
+        private void mainTab_SelectedIndexChanged(object sender, EventArgs e)      // Removes selections on tab change
+        {
+            clearAll();
+        }
+
+        private void clearAll() {                                                  // Function for clearing out all selections
+            selectedFood = "Nothing";
+
+            pizzaBox.SelectedIndex = -1;
+            pizzaBox.Text = "Wybierz pizzę...";
+
+            daniaBox.SelectedIndex = -1;
+            daniaBox.Text = "Wybierz danie...";
+
+            zupyBox.SelectedIndex = -1;
+            zupyBox.Text = "Wybierz zupę...";
+
+            napojeBox.SelectedIndex = -1;
+            napojeBox.Text = "Wybierz napój...";
+
+            foreach (int n in pizzaListBox.CheckedIndices)
+            {
+                pizzaListBox.SetItemCheckState(n, CheckState.Unchecked);
+            }
+
+            foreach (int i in daniaListBox.CheckedIndices)
+            {
+                daniaListBox.SetItemCheckState(i, CheckState.Unchecked);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
